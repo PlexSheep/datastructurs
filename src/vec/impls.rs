ff623de2e2c88777cdb083f5eab07e19877f76bb
Christoph J. Scherr <software@cscherr.de>
2025-06-05 23:09:44 +0200

    refactor(vec): iter stuff

---
+++ src/vec/impls.rs
@@ -0,0 +1,232 @@
+use std::marker::PhantomData;
+
+use super::*;
+
+pub(crate) struct RawIter<T> {
+    start: *const T,
+    end: *const T,
+}
+
+pub struct IntoIter<T> {
+    _data: RawVec<T>,
+    iter: RawIter<T>,
+}
+
+pub struct IntoIterRef<'a, T> {
+    vec: &'a Vec<T>,
+    index: usize,
+}
+
+pub struct Drain<'a, T: 'a> {
+    pub(crate) marker: PhantomData<&'a mut Vec<T>>,
+    pub(crate) iter: RawIter<T>,
+}
+
+impl<T> RawIter<T> {
+    pub(crate) unsafe fn new(slice: &[T]) -> Self {
+        RawIter {
+            start: slice.as_ptr(),
+            end: if slice.is_empty() {
+                slice.as_ptr()
+            } else {
+                unsafe { slice.as_ptr().add(slice.len()) }
+            },
+        }
+    }
+}
+
+impl<T> Iterator for RawIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        if self.start == self.end {
+            None
+        } else {
+            unsafe {
+                let result = ptr::read(self.start);
+                self.start = self.start.offset(1);
+                Some(result)
+            }
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = (self.end as usize - self.start as usize) / mem::size_of::<T>();
+        (len, Some(len))
+    }
+}
+
+impl<T> DoubleEndedIterator for RawIter<T> {
+    fn next_back(&mut self) -> Option<T> {
+        if self.start == self.end {
+            None
+        } else {
+            unsafe {
+                self.end = self.end.offset(-1);
+                Some(ptr::read(self.end))
+            }
+        }
+    }
+}
+
+impl<T> Index<usize> for Vec<T> {
+    type Output = T;
+
+    #[inline]
+    fn index(&self, index: usize) -> &Self::Output {
+        Index::index(&**self, index)
+    }
+}
+
+impl<T> IndexMut<usize> for Vec<T> {
+    #[inline]
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        IndexMut::index_mut(&mut **self, index)
+    }
+}
+
+impl<T> Deref for Vec<T> {
+    type Target = [T];
+    fn deref(&self) -> &[T] {
+        unsafe { std::slice::from_raw_parts(self.buf.ptr.as_ptr(), self.used) }
+    }
+}
+
+impl<T> DerefMut for Vec<T> {
+    fn deref_mut(&mut self) -> &mut [T] {
+        unsafe { std::slice::from_raw_parts_mut(self.buf.ptr.as_ptr(), self.used) }
+    }
+}
+
+impl<T> Drop for Vec<T> {
+    fn drop(&mut self) {
+        while self.pop().is_some() {}
+    }
+}
+
+impl<T> From<&[T]> for Vec<T> {
+    fn from(value: &[T]) -> Self {
+        Self::from_slice(value)
+    }
+}
+
+impl<T: PartialEq> PartialEq for Vec<T> {
+    fn eq(&self, other: &Self) -> bool {
+        if self.len() != other.len() {
+            return false;
+        }
+        for i in 0..self.len() {
+            if self[i] != other[i] {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+impl<T: Eq> Eq for Vec<T> {}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next()
+    }
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.iter.size_hint()
+    }
+}
+
+impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.iter.next_back()
+    }
+}
+
+impl<'a, T> Iterator for IntoIterRef<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.vec.len() {
+            let item = &self.vec[self.index];
+            self.index += 1;
+            Some(item)
+        } else {
+            None
+        }
+    }
+}
+
+impl<T> IntoIterator for Vec<T> {
+    type Item = T;
+    type IntoIter = IntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let iter = unsafe { RawIter::new(&self) };
+
+        let buf = unsafe { ptr::read(&self.buf) };
+        mem::forget(self);
+
+        IntoIter { iter, _data: buf }
+    }
+}
+
+impl<T> Drop for IntoIter<T> {
+    fn drop(&mut self) {
+        for _ in &mut *self {}
+    }
+}
+
+impl<'a, T> Iterator for Drain<'a, T> {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        self.iter.next()
+    }
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.iter.size_hint()
+    }
+}
+
+impl<'a, T> DoubleEndedIterator for Drain<'a, T> {
+    fn next_back(&mut self) -> Option<T> {
+        self.iter.next_back()
+    }
+}
+
+impl<'a, T> Drop for Drain<'a, T> {
+    fn drop(&mut self) {
+        for _ in &mut *self {}
+    }
+}
+
+impl<'a, T> IntoIterator for &'a Vec<T> {
+    type Item = &'a T;
+    type IntoIter = IntoIterRef<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        IntoIterRef {
+            vec: self,
+            index: 0,
+        }
+    }
+}
+
+impl<T> FromIterator<T> for Vec<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let mut vec = Vec::new();
+        for item in iter {
+            vec.push(item);
+        }
+        vec
+    }
+}
+
+impl<T> Extend<T> for Vec<T> {
+    fn extend<I: IntoIterator<Item = T>>(&mut self, iter: I) {
+        for item in iter {
+            self.push(item);
+        }
+    }
+}
+
+unsafe impl<T: Send> Send for Vec<T> {}
+unsafe impl<T: Sync> Sync for Vec<T> {}
