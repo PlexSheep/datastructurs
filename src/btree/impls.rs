34a27350c80fe59420989589b9e3a742cac0f67f
Christoph J. Scherr <software@cscherr.de>
2025-06-06 17:04:33 +0200

    refactor(btree): split module and make generics clear

---
+++ src/btree/impls.rs
@@ -0,0 +1,71 @@
+use std::marker::PhantomData;
+
+use super::{BTree, Node, NodePtr, deref_node, deref_node_mut};
+
+impl<T: Ord> Drop for Node<T> {
+    fn drop(&mut self) {
+        for child_ptr in &self.children {
+            Node::drop(*child_ptr);
+        }
+    }
+}
+
+impl<T: Ord + Clone> Drop for BTree<T> {
+    fn drop(&mut self) {
+        Node::drop(self.root);
+    }
+}
+
+// Simple iterator implementation
+pub struct BTreeIter<'a, T: Ord> {
+    stack: Vec<(NodePtr<T>, usize)>,
+    marker: PhantomData<&'a ()>,
+}
+
+impl<'a, T: Ord> BTreeIter<'a, T> {
+    pub(crate) fn new(root_ptr: &'a NodePtr<T>) -> Self {
+        let mut iter = BTreeIter {
+            stack: Vec::new(),
+            marker: PhantomData,
+        };
+        iter.push_left_path(root_ptr, 0);
+        iter
+    }
+
+    fn push_left_path(&mut self, node_ptr: &'a NodePtr<T>, start_idx: usize) {
+        let mut node = deref_node_mut(node_ptr);
+        loop {
+            self.stack.push((node.as_ptr(), start_idx));
+            if node.is_leaf() {
+                break;
+            }
+            node = deref_node_mut(&node.children[start_idx]);
+        }
+    }
+}
+
+impl<'a, T: Ord + 'a> Iterator for BTreeIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some((node_ptr, idx)) = self.stack.pop() {
+            let node = deref_node(node_ptr);
+            if idx < node.keys.len() {
+                let key = &node.keys[idx];
+
+                // Prepare for next iteration
+                if !node.is_leaf() && idx + 1 < node.children.len() {
+                    self.push_left_path(&node.children[idx + 1], 0);
+                }
+
+                // Push current node back with incremented index
+                if idx + 1 < node.keys.len() {
+                    self.stack.push((node_ptr, idx + 1));
+                }
+
+                return Some(key);
+            }
+        }
+        None
+    }
+}
--- src/btree/mod.rs
+++ src/btree/mod.rs
@@ -1,21 +1,25 @@
-use std::{marker::PhantomData, mem, ptr::NonNull};
+use std::{mem, ptr::NonNull};
+
+use impls::BTreeIter;
 
 use crate::vec::Vec;
+
+mod impls;
 
 pub const DEFAULT_BRANCH_FACTOR: usize = 100;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-struct Node<T: Ord> {
+pub(crate) struct Node<T: Ord> {
     keys: Vec<T>,
     parent: Option<NodePtr<T>>,
     children: Vec<NodePtr<T>>,
 }
 
-type NodePtr<T> = NonNull<Node<T>>;
-type OpNodePtr<T> = Option<NodePtr<T>>;
+pub(crate) type NodePtr<T> = NonNull<Node<T>>;
+pub(crate) type OpNodePtr<T> = Option<NodePtr<T>>;
 
 #[derive(Clone, Debug)]
-pub struct BTree<T: Ord> {
+pub struct BTree<T: Ord + Clone> {
     root: NodePtr<T>,
     props: BTreeProperties,
 }
@@ -57,7 +61,7 @@ impl BTreeProperties {
         }
     }
 
-    fn split_child<T: Ord + Clone>(&self, parent_ptr: NodePtr<T>, child_index: usize) {
+    fn split_child<T: Ord>(&self, parent_ptr: NodePtr<T>, child_index: usize) {
         let parent = deref_node_mut(&parent_ptr);
         let child = deref_node_mut(&parent.children[child_index]);
 
@@ -90,7 +94,7 @@ impl BTreeProperties {
         }
     }
 
-    fn insert_non_full<T: Ord + Clone>(&self, node_ptr: NodePtr<T>, key: T) {
+    fn insert_non_full<T: Ord>(&self, node_ptr: NodePtr<T>, key: T) {
         let node = deref_node_mut(&node_ptr);
         let index = Self::find_insertion_index(&node.keys, &key);
 
@@ -524,74 +528,6 @@ impl<T: Ord + Clone> BTree<T> {
             current = deref_node_ref(&current.children[0]);
         }
         current.keys[0].clone()
-    }
-}
-
-impl<T: Ord> Drop for Node<T> {
-    fn drop(&mut self) {
-        for child_ptr in &self.children {
-            Node::drop(*child_ptr);
-        }
-    }
-}
-
-impl<T: Ord> Drop for BTree<T> {
-    fn drop(&mut self) {
-        Node::drop(self.root);
-    }
-}
-
-// Simple iterator implementation
-pub struct BTreeIter<'a, T: Ord> {
-    stack: Vec<(NodePtr<T>, usize)>,
-    marker: PhantomData<&'a ()>,
-}
-
-impl<'a, T: Ord> BTreeIter<'a, T> {
-    fn new(root_ptr: &'a NodePtr<T>) -> Self {
-        let mut iter = BTreeIter {
-            stack: Vec::new(),
-            marker: PhantomData,
-        };
-        iter.push_left_path(root_ptr, 0);
-        iter
-    }
-
-    fn push_left_path(&mut self, node_ptr: &'a NodePtr<T>, start_idx: usize) {
-        let mut node = deref_node_mut(node_ptr);
-        loop {
-            self.stack.push((node.as_ptr(), start_idx));
-            if node.is_leaf() {
-                break;
-            }
-            node = deref_node_mut(&node.children[start_idx]);
-        }
-    }
-}
-
-impl<'a, T: Ord + 'a> Iterator for BTreeIter<'a, T> {
-    type Item = &'a T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        while let Some((node_ptr, idx)) = self.stack.pop() {
-            let node = deref_node(node_ptr);
-            if idx < node.keys.len() {
-                let key = &node.keys[idx];
-
-                // Prepare for next iteration
-                if !node.is_leaf() && idx + 1 < node.children.len() {
-                    self.push_left_path(&node.children[idx + 1], 0);
-                }
-
-                // Push current node back with incremented index
-                if idx + 1 < node.keys.len() {
-                    self.stack.push((node_ptr, idx + 1));
-                }
-
-                return Some(key);
-            }
-        }
-        None
     }
 }
 
